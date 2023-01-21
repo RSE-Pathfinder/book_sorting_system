66966311120f8d0266747871a26af0958e55aee6
Muhammad Syamim <Syazam33@gmail.com>
2023-01-21 15:53:27 +0800

    Update for hardware test

--- bss_bringup/launch/mobile_shelf_gazebo.launch.py
+++ bss_bringup/launch/mobile_shelf_gazebo.launch.py
@@ -15,48 +15,31 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     # Constants for paths to different files and folders
     gazebo_models_path = 'models'
-    package_name = 'scout_description'
-    robot_name_in_model = 'scout_v2'
+    model_package_name = 'scout_description'
     rviz_config_file_path = 'rviz/scout_v2.rviz'
     urdf_file_path = 'urdf/scout_v2.urdf'
     world_file_path = 'worlds/empty.world'
-
-    # Pose where we want to spawn the robot
-    spawn_x_val = '0.0'
-    spawn_y_val = '0.0'
-    spawn_z_val = '0.0'
-    spawn_yaw_val = '0.00'
+    package_name = 'bss_bringup'
 
 ############ You do not need to change anything below this line #############
 
     # Set the path to different files and folders.  
     pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+    model_pkg_share = FindPackageShare(package=model_package_name).find(model_package_name)
     pkg_share = FindPackageShare(package=package_name).find(package_name)
-    default_urdf_model_path = os.path.join(pkg_share, urdf_file_path)
-    default_rviz_config_path = os.path.join(pkg_share, rviz_config_file_path)
-    world_path = os.path.join(pkg_share, world_file_path)
-    gazebo_models_path = os.path.join(pkg_share, gazebo_models_path)
+    default_urdf_model_path = os.path.join(model_pkg_share, urdf_file_path)
+    default_rviz_config_path = os.path.join(model_pkg_share, rviz_config_file_path)
+    world_path = os.path.join(model_pkg_share, world_file_path)
+    gazebo_models_path = os.path.join(model_pkg_share, gazebo_models_path)
     os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
 
     # Launch configuration variables specific to simulation
-    gui = LaunchConfiguration('gui')
     headless = LaunchConfiguration('headless')
-    namespace = LaunchConfiguration('namespace')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
-    urdf_model = LaunchConfiguration('urdf_model')
-    use_namespace = LaunchConfiguration('use_namespace')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
-    use_rviz = LaunchConfiguration('use_rviz')
-    use_sim_time = LaunchConfiguration('use_sim_time')
     use_simulator = LaunchConfiguration('use_simulator')
     world = LaunchConfiguration('world')
     
-    # Declare the launch arguments  
-    declare_use_joint_state_publisher_cmd = DeclareLaunchArgument(
-    name='gui',
-    default_value='True',
-    description='Flag to enable joint_state_publisher_gui')
-
+    # Declare the launch arguments
     declare_namespace_cmd = DeclareLaunchArgument(
     name='namespace',
     default_value='',
@@ -82,11 +65,6 @@ def generate_launch_description():
     default_value=default_urdf_model_path, 
     description='Absolute path to robot urdf file')
 
-    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
-    name='use_robot_state_pub',
-    default_value='True',
-    description='Whether to start the robot state publisher')
-
     declare_use_rviz_cmd = DeclareLaunchArgument(
     name='use_rviz',
     default_value='True',
@@ -106,33 +84,6 @@ def generate_launch_description():
     name='world',
     default_value=world_path,
     description='Full path to the world model file to load')
-    
-    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.    
-    start_robot_state_publisher_cmd = Node(
-    package='robot_state_publisher',
-    executable='robot_state_publisher',
-    parameters=[{'robot_description': Command(['xacro ', urdf_model])}])
-
-    # Publish the joint states of the robot
-    start_joint_state_publisher_cmd = Node(
-    package='joint_state_publisher',
-    executable='joint_state_publisher',
-    name='joint_state_publisher',
-    condition=UnlessCondition(gui))
-
-    #ROS Control Joint State Controller
-    load_joint_state_controller_cmd = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    #ROS Control trajectory controller
-    start_joint_trajectory_controller_cmd = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
-             'diff_drive_base_controller'],
-        output='screen'
-    )
 
     # Launch RViz
     start_rviz_cmd = Node(
@@ -153,39 +104,22 @@ def generate_launch_description():
     PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
     condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
 
-    # Spawn the robot
-    spawn_entity_cmd = Node(
-    package='gazebo_ros', 
-    executable='spawn_entity.py',
-    arguments=['-entity', robot_name_in_model, 
-    '-topic', 'robot_description',
-    '-x', spawn_x_val,
-    '-y', spawn_y_val,
-    '-z', spawn_z_val,
-    '-Y', spawn_yaw_val],
-    output='screen')
-
-    model_name = 'scout_v2.xacro'
-    # model_path = os.path.join(get_package_share_directory('scout_description'), "urdf", model_name)
-    # print(model_path)
-    robot_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
-        PathJoinSubstitution(
-            [FindPackageShare("scout_description"), "urdf", model_name]
-        ),
-    ])
+    # Start Scout Spawning Sequence
+    start_spawner_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_share, 'launch', 'mobile_shelf_spawner.launch.py')
+        )
+    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
     # Declare the launch options
-    ld.add_action(declare_use_joint_state_publisher_cmd)
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_namespace_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_simulator_cmd)
     ld.add_action(declare_urdf_model_path_cmd)
-    ld.add_action(declare_use_robot_state_pub_cmd)  
     ld.add_action(declare_use_rviz_cmd) 
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_use_simulator_cmd)
@@ -194,22 +128,7 @@ def generate_launch_description():
     # Add any actions
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
-    ld.add_action(spawn_entity_cmd)
-    ld.add_action(
-        launch_ros.actions.Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time'),
-                'robot_description':robot_description_content
-            }])
-    )
-    ld.add_action(start_robot_state_publisher_cmd)
-    ld.add_action(start_joint_state_publisher_cmd)
-    ld.add_action(load_joint_state_controller_cmd)
-    ld.add_action(start_joint_trajectory_controller_cmd)
-    ld.add_action(start_rviz_cmd)
+    #ld.add_action(start_rviz_cmd)
+    ld.add_action(start_spawner_cmd)
     
     return ld
