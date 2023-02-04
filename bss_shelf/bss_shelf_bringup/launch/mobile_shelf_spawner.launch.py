1c4f0c992e42149cbaa0612d4cb56041bc84baca
Muhd <Syazam33@gmail.com>
2023-02-04 16:12:59 +0800

    Fixed teleop

--- bss_shelf/bss_shelf_bringup/launch/mobile_shelf_spawner.launch.py
+++ bss_shelf/bss_shelf_bringup/launch/mobile_shelf_spawner.launch.py
@@ -1,5 +1,3 @@
-import os
-
 from launch import LaunchDescription
 from launch.substitutions import Command, PathJoinSubstitution, FindExecutable
 
@@ -10,16 +8,13 @@ def generate_launch_description():
     # Constants for paths to different files and folders
     package_name = 'wrp_ros2'
     robot_name_in_model = 'scout_v2'
-    urdf_file_path = 'urdf/scout_v2/scout_v2.xacro'
+    urdf_file_path = 'urdf/scout_v2/scout_v2_nav.xacro'
 
     # Pose where we want to spawn the robot
-    spawn_x_val = '2.0'
-    spawn_y_val = '12.0'
+    spawn_x_val = '0.0'
+    spawn_y_val = '0.0'
     spawn_z_val = '0.0'
-    spawn_yaw_val = '-1.50'
-
-    # Set the path to different files and folders.
-    config_file = "config/scout_v2_control.yaml"
+    spawn_yaw_val = '0.0'
 
     # Extract URDF using XACRO 
     robot_description_content = Command(
@@ -33,43 +28,18 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
-    # Extract Config file
-    scout_diff_drive_controller = PathJoinSubstitution(
-        [FindPackageShare("bss_shelf_bringup"), config_file],
+    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
     )
 
-    # Controller node
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_description, scout_diff_drive_controller],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-    )
-
-    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.    
+    #Robot State   
     robot_state_publisher = Node(
     package='robot_state_publisher',
     executable='robot_state_publisher',
     parameters=[robot_description],
-    )
-
-    # ROS2 Control Joint Broadcaster Controller
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["joint_state_broadcaster"],
-        output="screen",
-    )
-
-    # ROS2 Control diff drive controller
-    robot_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=["diff_drive_base_controller"],
-        output="screen",
     )
 
     # Spawn the robot
@@ -86,11 +56,9 @@ def generate_launch_description():
 
     # Add any actions
     nodes = [
-        #control_node,
         robot_state_publisher,
         robot_entity_spawner,
-        joint_state_broadcaster_spawner,
-        robot_controller_spawner,
+        joint_state_publisher_node,
     ]
     
     return LaunchDescription(nodes)
